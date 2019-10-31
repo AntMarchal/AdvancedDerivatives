9c7f7035c3eb2d934862d5194d7413b819ea2d30
valentintissot <valentin.tissotdaguette@gmail.com>
2019-10-31 20:45:37 +0100

    PS7

---
+++ PS7/BMT_PS7.m
@@ -0,0 +1,77 @@
+%=========================================================================%
+%========================== Advanced Derivatives =========================% 
+%============================== Problem Set 7 ============================%
+%======== BRODARD Lionel, MARCHAL Antoine, TISSOT-DAGUETTE Valentin ======%
+%=========================================================================%
+
+close all; clear; clc; format short; warning('off')
+
+% Freeze the random seed
+rng(1)
+
+%% 0. Setup
+
+S_0 = 100; K = 98; dt = 1/4; t = dt:dt:1; N_t = length(t);
+
+r = 0; q = 0.02; sigma = 0.23; N_MC = 10.^(2:5); N_sim = 1e2;
+
+Price = zeros(length(N_MC),2); Std = zeros(length(N_MC),2);
+
+%% I. Least-Square Monte Carlo with constant volatility
+
+for j = 1:length(N_MC)
+    
+% Please refer to the function LSMC_Price for further details
+[Price(j,1),Std(j,1)] = LSMC(S_0,K,r,q,sigma,t,t,dt,N_MC(j),N_sim);
+end
+
+%% II. Least-Square Monte Carlo with Local Volatilities
+
+% Get the adequate local volatilities from the previous assignment 
+data = readtable('Volatility_Surface.csv');
+
+Local_Vol = data{data{:,'K_over_S'} == K/S_0,2:end}';
+
+N_Vol = length(Local_Vol);
+
+% Time to maturities from the volatility surface
+TTM = zeros(N_Vol ,1);
+
+for j = 1:N_Vol
+    
+    var_name = data.Properties.VariableNames{j+1};
+    
+    TTM(j) = str2double(var_name(3:end))/1e3;
+end
+
+% Use monthly intervals as time grid
+dt_M = 1/12; t_M = round(dt_M:dt_M:1,12);
+
+% Interpolate the local volatilities on the time grid
+sigma_M = interp1(TTM,Local_Vol,t_M);
+
+for j = 1:length(N_MC)
+    
+[Price(j,2),Std(j,2)] = LSMC(S_0,K,r,q,sigma_M,t_M,t,dt_M,N_MC(j),N_sim);
+end
+
+%% III. Results
+
+vol = {'constant volatility','local volatilities'};
+
+for m = 1:2
+fprintf('\nLeast-Square Monte Carlo with %s\n',vol{m});
+fprintf('\nPrice with %d MC simulations: %2.4f\n',N_MC(end),Price(end,m))
+fprintf('\nCorresponding standard deviation: %2.4f\n',Std(end,m))
+fprintf('\n___________________________________________________\n');
+end
+
+figure 
+loglog(N_MC,Std,'o-','Linewidth',1.5); grid on; hold on
+loglog(N_MC,exp(2)./sqrt(N_MC),'k--','Linewidth',1.5)
+legend('LSMC with constant volatility',...
+       'LSMC with local volatilies'   ,...
+       " O(N_{MC}^{-1/2})" )
+xlabel('Number of Monte Carlo Simulations')
+ylabel('Standard deviation')
+title('Standard deviation of the option price versus N_{MC} ')
---
+++ PS7/LSMC.m
@@ -0,0 +1,83 @@
+function [Price,Std] = LSMC(S_0,K,r,q,sigma,t,t_ex,dt,N_MC,N_sim)
+
+%=========================================================================%
+% Least-Square Monte Carlo Method                                         %
+%=========================================================================%
+% INPUT:                                                                  %
+% S_0   : Initial Stock Price                                             %
+% K     : Strike                                                          %
+% r     : Interest Rate                                                   %
+% q     : Dividend Yield                                                  %
+% sigma : Array of local volatilities                                     %
+% t     : Simulation dates                                                %
+% t_ex  : Exercise dates                                                  %
+% dt    : Time step for the simulations                                   %
+% N_MC  : Number of Monte Carlo simulations                               %
+% N_sim : Number of simulations to compute the standard deviation         %
+%=========================================================================%
+
+% Number of simulation dates
+N_t = length(t); 
+
+% Number of exercise dates
+N_ex = length(t_ex);
+
+% Array of option prices
+Prices = zeros(N_sim,1);
+
+for s = 1:N_sim 
+    
+% Trajectories
+S = S_0 * cumprod(1 + (r-q)*dt+ sqrt(dt)*sigma.*randn(N_MC,N_t),2);
+
+% Moving average of the stock prices
+A = movmean(S,[N_t 0],2);
+
+% Indices of t where one can exercise the option
+id_ex = find(ismember(t,t_ex));
+
+% Keep only the (average) stock prices at the exercise dates
+S = S(:,id_ex); A = A(:,id_ex);
+
+% Matrix of intrinsic values
+Int_Values = max(A - K,0);
+
+% Cash Flow Matrix 
+CF = zeros(N_MC,N_ex);
+
+% Store the payouts at maturity
+CF(:,end) = Int_Values(:,end);
+
+for k = N_ex-1:-1:1
+    
+    % Row indices where the intrisic value is positive at time k
+    id = find(Int_Values(:,k) > 0);
+    
+    % Design Matrix
+    X = [ones(length(id),1),S(id,k),S(id,k).^2,S(id,k).^3,...
+                A(id,k),A(id,k).^2,A(id,k).^3];
+
+    % Response Variable
+    Y = sum(exp(-r *(t_ex(k+1:end) - t_ex(k))) .* CF(id,k+1:end),2);
+
+    % Continuation values obtained using OLS regression
+    Cont_Values =  X * ((X'*X)\(X'*Y));
+    
+    % Get the indices where it is better to exercise
+    id_ex = find(Int_Values(id,k) > Cont_Values);
+    
+    % Add the corresponding intrisic values in the cash flow matrix
+    CF(id(id_ex),k) = Int_Values(id(id_ex),k);
+    
+    % Set all the future cash flows to 0 when we exercise at time k
+    CF(id(id_ex),k+1:end) = 0;
+    
+end
+
+% Average over all paths to get the price of the option  
+Prices(s) = mean(sum(CF .* exp(- r * t_ex),2));
+
+end
+
+% Compute the average price and its standard deviation
+Price = mean(Prices);   Std = std(Prices) ;
